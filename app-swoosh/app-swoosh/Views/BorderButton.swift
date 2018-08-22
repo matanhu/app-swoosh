1b5b470b1d35e494ea0ef4dbf469d0b481c2dbea
MatanHu <MatanHuja@gmail.com>
2018-08-22 23:45:53 +0300

    Create Welcom Screen

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by MatanHuja on 22/08/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
