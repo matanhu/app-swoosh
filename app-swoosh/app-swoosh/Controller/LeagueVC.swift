a278f1bf191cb4150d6bd723ed1917773da7f336
MatanHu <MatanHuja@gmail.com>
2018-08-29 22:41:13 +0300

    Loading Segue from code by "performSegue" function

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -21,6 +21,10 @@ class LeagueVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func oneNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
 
     /*
     // MARK: - Navigation
