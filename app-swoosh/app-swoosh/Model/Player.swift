45cd78b5529266cf3631d86f8ecef23dd78c68b7
MatanHu <MatanHuja@gmail.com>
2018-08-31 18:16:04 +0300

    IBActions (handling events) & data models And Passing data between view controllers

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by MatanHuja on 31/08/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
